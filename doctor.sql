3d07b4d23c8706d2bccc473757ed72da38306fe9
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-08-10 10:32:45 +0530

    features: implements a normalize database design for doctor database

---
+++ doctor.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: doctor
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctor` (
+  `doctor_id` int(11) NOT NULL,
+  `doctor_name` varchar(50) DEFAULT NULL,
+  `secretary_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`doctor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctor`
+--
+
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+INSERT INTO `doctor` VALUES (1,'neelufar','aasif'),(2,'neha','shekhar'),(3,'heba','sane'),(4,'shumaila','areeb');
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `drug`
+--
+
+DROP TABLE IF EXISTS `drug`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `drug` (
+  `drug_id` int(11) NOT NULL,
+  `drug_name` varchar(50) DEFAULT NULL,
+  `dosage` varchar(50) DEFAULT NULL,
+  `prescription` int(11) DEFAULT NULL,
+  PRIMARY KEY (`drug_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drug`
+--
+
+LOCK TABLES `drug` WRITE;
+/*!40000 ALTER TABLE `drug` DISABLE KEYS */;
+INSERT INTO `drug` VALUES (301,'amlodipine','one tab when chest pain',204),(302,'sinelee','twice in a day',201),(303,'grillinctus','two spoon after every meal',204),(304,'spasmo','once before bed',203),(305,'aspirin','one when figure and headache',202),(306,'glucose','twice in a day',203),(307,'livocitrezen','thrice befor meal',204),(308,'sumo','once before breakfast',203),(309,'hytrin','once before dinner',202),(310,'sumo','twice in a day',201),(311,'crocin','once befor bed',204),(312,'pan-d','once in the morning',203),(313,'paracetamol','thrice in a day after meal',204);
+/*!40000 ALTER TABLE `drug` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `patient_id` int(11) NOT NULL,
+  `patient_name` varchar(50) DEFAULT NULL,
+  `patient_dob` date DEFAULT NULL,
+  `patient_address` varchar(100) DEFAULT NULL,
+  `doctor_id` int(11) DEFAULT NULL,
+  `prescription` int(11) DEFAULT NULL,
+  `appointment_date` date DEFAULT NULL,
+  PRIMARY KEY (`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES (101,'aquif','1998-02-12','koramangla',1,201,'2020-01-01'),(102,'waquar','1995-08-01','aligarh',2,202,'2020-02-02'),(103,'nasir','1995-08-06','hajipur',3,203,'2020-03-03'),(104,'khalid','1994-06-15','balia',2,204,'2020-04-04');
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-10 10:32:17
