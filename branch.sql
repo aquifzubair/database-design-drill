500697e22c6d1fc1fc70de5dcf4029c8e464b32e
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-08-16 07:58:13 +0530

    refactor: adds foriegn key in branch tables and make it more readable

--- branch.sql
+++ branch.sql
@@ -1,102 +1,45 @@
--- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
---
--- Host: localhost    Database: branch
--- ------------------------------------------------------
--- Server version	5.7.31-0ubuntu0.18.04.1
+CREATE TABLE branch
+(
+    branch_id INT PRIMARY KEY,
+    branch_name VARCHAR(100)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `book_details`
---
+CREATE TABLE branch_address
+(
+    address_id INT PRIMARY KEY,
+    branch_address VARCHAR(100),
+    branch_id INT,
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id)
+);
 
-DROP TABLE IF EXISTS `book_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `book_details` (
-  `title` varchar(100) NOT NULL,
-  `author` varchar(50) DEFAULT NULL,
-  `publisher` varchar(100) DEFAULT NULL,
-  `ISBN` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `book_details`
---
+CREATE TABLE book_details
+(
+    book_id INT PRIMARY KEY,
+    book_title VARCHAR(100),
+    isbn BIGINT,
+    num_of_copies INT,
+    branch_id INT,
+    publisher_id INT,
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id),
+    FOREIGN KEY(pulisher_id) REFERENCES publisher_details(publisher_id)
+);
 
-LOCK TABLES `book_details` WRITE;
-/*!40000 ALTER TABLE `book_details` DISABLE KEYS */;
-INSERT INTO `book_details` VALUES ('how to win friends and influence people','dale carnegie','simon & schuster',9788192910994),('revolution 2020','chetan bhagat','rupa publication',9780141033570),('thinking fast and slow','daniel kahneman','farrar, straus and giroux',9780141033570);
-/*!40000 ALTER TABLE `book_details` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `branch`
---
+CREATE TABLE publisher_details
+(
+    publisher_id INT PRIMARY KEY,
+    publisher_name VARCHAR(100),
+    book_id INT,
+    FOREIGN KEY(book_id) REFERENCES book_details(book_id)
+);
 
-DROP TABLE IF EXISTS `branch`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `branch` (
-  `branch_id` int(11) NOT NULL AUTO_INCREMENT,
-  `branch_address` varchar(100) DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `branch`
---
-
-LOCK TABLES `branch` WRITE;
-/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
-INSERT INTO `branch` VALUES (1,'koramangla 5th block','how to win friends and influence people'),(2,'koramangla 6th block','thinking fast and slow'),(3,'east anwarpur','revolution 2020');
-/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `isbn_details`
---
-
-DROP TABLE IF EXISTS `isbn_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `isbn_details` (
-  `ISBN` bigint(20) NOT NULL,
-  `num_copies` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ISBN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `isbn_details`
---
-
-LOCK TABLES `isbn_details` WRITE;
-/*!40000 ALTER TABLE `isbn_details` DISABLE KEYS */;
-INSERT INTO `isbn_details` VALUES (9780141033570,30000),(9788192910994,20000),(9789863546278,10000);
-/*!40000 ALTER TABLE `isbn_details` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-08-09 15:13:21
+CREATE TABLE autor_detail
+(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(100),
+    book_id INT,
+    FOREIGN KEY(book_id) REFERENCES book(book_id)
+);
