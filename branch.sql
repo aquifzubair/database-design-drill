eca3e3807d10acef95bb138c001c340b7846e378
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-08-18 12:21:48 +0530

    bugfix: creates two table to connect with branch and, books and author and books. Removes circular references

--- branch.sql
+++ branch.sql
@@ -20,9 +20,7 @@ CREATE TABLE book_details
     book_title VARCHAR(100),
     isbn BIGINT,
     num_of_copies INT,
-    branch_id INT,
     publisher_id INT,
-    FOREIGN KEY(branch_id) REFERENCES branch(branch_id),
     FOREIGN KEY(pulisher_id) REFERENCES publisher_details(publisher_id)
 );
 
@@ -30,16 +28,30 @@ CREATE TABLE book_details
 CREATE TABLE publisher_details
 (
     publisher_id INT PRIMARY KEY,
-    publisher_name VARCHAR(100),
-    book_id INT,
-    FOREIGN KEY(book_id) REFERENCES book_details(book_id)
+    publisher_name VARCHAR(100)
 );
 
 
 CREATE TABLE autor_detail
 (
     author_id INT PRIMARY KEY,
-    author_name VARCHAR(100),
-    book_id INT,
-    FOREIGN KEY(book_id) REFERENCES book(book_id)
+    author_name VARCHAR(100)
 );
+
+
+CREATE TABLE book_author 
+(
+    book_id INT,
+    author_id INT,
+    FOREIGN KEY(author_id) REFERENCES author_detail(author_id),
+    FOREIGN KEY(book_id) REFERENCES book_details(book_id),
+)
+
+
+CREATE TABLE books_in_branch 
+(
+    book_id INT,
+    branch_id INT,
+    FOREIGN KEY(book_id) REFERENCES book_details(book_id),
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) 
+)
